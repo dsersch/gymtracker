5d2feef5a80d75fa2c48290082320987c3b1f91e
David Sersch <dsersch640@gmail.com>
2017-09-25 14:54:47 -0700

    login and sign up forms

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,10 +8,18 @@ class UsersController < ApplicationController
   end
 
   def new
-    
+    @user = User.new
   end
 
   def create
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to user_path(@user)
+    else
+      flash[:warning] = "Please complete all fields."
+      redirect_to new_user_path
+    end
   end
 
   def edit
@@ -21,5 +29,10 @@ class UsersController < ApplicationController
   end
 
   def destroy
+  end
+  
+  private
+  def user_params 
+    params.require(:user).permit(:name, :email, :password, :password_confrimation)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   root 'users#index'
   resources :users
+  resources :sessions, only: [:new, :create]
+  delete "/logout" => "sessions#destroy", as: :logout
 end
