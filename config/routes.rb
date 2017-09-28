1ba21bebbf89416017949d3f2f7c982f3cc1b32e
David Sersch <dsersch640@gmail.com>
2017-09-28 15:15:41 -0700

    homepage change

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  root 'users#index'
+  root 'sessions#new'
   resources :users
   resources :sessions, only: [:new, :create]
   delete "/logout" => "sessions#destroy", as: :logout
